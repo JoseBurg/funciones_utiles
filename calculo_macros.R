2dd38c52bf1f1dc5206f82b1a1e12874bf1d102f
José Antonio Burgos Francisco <65372353+JoseBurg@users.noreply.github.com>
2023-05-30 10:49:40 -0400

    Update calculo_macros.R
    variacion porcentual

--- calculo_macros.R
+++ calculo_macros.R
@@ -7,3 +7,12 @@ data_by_variable |>
     variacion = 
     across(-periodo, \(x){x -lag(x)})
     )
+
+# Variacion porcentual
+data_by_variable |> 
+  select(periodo, pais, tpm) |> 
+  pivot_wider(names_from = pais, values_from = tpm) |> 
+  mutate(
+    variacion = 
+    across(-periodo, \(x){((x -lag(x))/x)*100})
+    )
