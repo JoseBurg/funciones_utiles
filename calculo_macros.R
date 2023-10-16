cbaa3215e55b56de033e7d5808f89459a4dba5eb
José Antonio Burgos Francisco <65372353+JoseBurg@users.noreply.github.com>
2023-10-16 17:07:45 -0400

    Update calculo_macros.R

--- calculo_macros.R
+++ calculo_macros.R
@@ -16,3 +16,15 @@ data_by_variable |>
     variacion = 
     across(-periodo, \(x){((x -lag(x))/x)*100})
     )
+
+
+# Aplicar filtro por columna
+df <- data.frame(
+  id = c(1, 2, 3, 4, 5, 6),
+  x = c(0, 0, 0, 0, 1, 0),
+  y = c(0, 0, 0, 1, 2, 0),
+  z = c(0, 0, 4, 0, 0, 0)
+  )
+
+df |> 
+  filter(if_any(-id, \(x){x != 0}))
